126f2f769a7e0e90549edbbeb7532a701f94c7c4
Balicholo <wisdombalicholo@gmail.com>
2024-08-26 15:47:33 +0200

    logo-link

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ function Header() {
     return (
         <nav className='navbar'>
             <div className='logo'>
-                <Link to='/' className='logo-link'>#CarRental</Link>
+                <Link to='https://react-travel-app-kappa.vercel.app/' className='logo-link'>#CarRental</Link>
             </div>
             <div className='nav-links'>
                 <Link to='/' className={location.pathname === "/" ? "active" : ""} style={{ textDecoration: location.pathname === "/" ? "underline" : "none" }}>Host</Link>
